d93e279bf3c8b9c8fd426731f482f843fd96414a
prasadonline <me@prasadonline.com>
2022-04-27 21:23:13 +0530

    Task 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/*
+ * Task 1
+ */
+
+function repeat(array $num){
+	$newString = '';
+	$count = 3;
+
+	$comma_separated = implode(",", $num);
+	for($i=0 ; $i<$count ; $i++){
+		$newString .= $comma_separated;
+		
+		if($i == $count-1){
+			$newString = '[ '.$newString.' ]';
+		}else{
+		    $newString .= ',';
+		}
+	}
+	echo $newString; 
+}
+
+repeat([1,2,3]);
+
+echo '<br><br>';
